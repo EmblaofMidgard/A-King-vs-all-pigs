5aeb9435c75e5322ce63e9f99cee1841240de263
EmblaofMidgard <evaluna95@live.it>
2023-01-24 18:12:05 +0100

    NemiciInOrdine

--- Assets/Scripts/LevelEnd.cs
+++ Assets/Scripts/LevelEnd.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelEnd : MonoBehaviour
-{
+{ 
+
+
+
     // Start is called before the first frame update
-    void Start()
+    public void NextRoom()
     {
-        
+       
+    
+            SceneManager.LoadScene("LivelloProva");
+     
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   
 }
